dd08f238de6ebe7b096f2076f3101178e5f6fda2
Dark99965 <noah.retter@icloud.com>
2026-06-21 17:25:41 -0300

    IMPROVED: varables + concat/script.pl "cleaned the code up"

--- varables + concat/script.pl
+++ varables + concat/script.pl
@@ -8,4 +8,4 @@ my $lastname = "Retter";
 my $age = 10.5;
 my $job = "Softwere dev";
 
-print "Hello world. My name is" . $name . $lastname . "and my age is " . $age . ".\nWhen i grow up i relley want to be a " . $job . ".\n";
+print "Hello world. My name is $name $lastname and my age is $age \nWhen i grow up i relley want to be a  $job \n";
