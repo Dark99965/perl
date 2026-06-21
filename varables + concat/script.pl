2cb0a38619827517bb51c18d587190a629b7ba9b
Dark99965 <noah.retter@icloud.com>
2026-06-21 17:20:56 -0300

    varables + concat

---
+++ varables + concat/script.pl
@@ -0,0 +1,11 @@
+#!/bin/perl
+
+use strict;
+use warnings;
+
+my $name = "Noah ";
+my $lastname = "Retter";
+my $age = 10.5;
+my $job = "Softwere dev";
+
+print "Hello world. My name is" . $name . $lastname . "and my age is " . $age . ".\nWhen i grow up i relley want to be a " . $job . ".\n";
